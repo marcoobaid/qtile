5c66b10d7ea024ff3a323c88eaea4d091da4d0aa
Marco Obaid <48951325+marcoobaid@users.noreply.github.com>
2019-04-22 21:16:16 -0500

    Add files via upload

---
+++ scripts/mylayout2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1920x1080 --pos 1920x0 --rotate normal
+qtile-cmd -o cmd -f restart
